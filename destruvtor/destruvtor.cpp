b1fe8c9411c7ce3dfe6db41eb9df7a9f8f61310c
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-05-17 18:30:27 +0700

    header + class mahasiswa

--- destruvtor/destruvtor.cpp
+++ destruvtor/destruvtor.cpp
@@ -11,37 +11,3 @@ public:
 	void cetakData();
 	void isiData();
 };
-//Definisi member Function
-angka::angka(int i) { //Constructor
-	panjang = i;
-	arr = new int[i];
-	isiData();
-}
-
-angka::~angka() { //Destructor
-	cout << endl;
-	cetakData();
-	delete[]arr;
-	cout << "Alamat Array Sudah Dilepaskan" << endl;
-}
-
-void angka::cetakData() {
-	for (int i = 1; i <= panjang; i++) {
-		cout << i << " = " << arr[i] << endl;
-	}
-}
-
-void angka::isiData() {
-	for (int i = 1; i <= panjang; i++) {
-		cout << i << " = "; cin >> arr[i];
-	}
-	cout << endl;
-}
-
-int main() {
-	angka belajarcpp(3); //constructor dipanggil
-	angka* ptrBelajarcpp = new angka(5); // Constructor dipanggil
-	delete ptrBelajarcpp; //Destructor dipanggil
-
-	return 0;
-} //Destructor dipanggil
